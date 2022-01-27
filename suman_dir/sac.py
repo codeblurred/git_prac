1544600be66f079582d84fd94bf1aef2fed5c86b
suman0788 <44658215+suman0788@users.noreply.github.com>
2022-01-27 01:12:55 -0800

    sac.py

---
+++ suman_dir/sac.py
@@ -0,0 +1,54 @@
+a = int(input("enter the value in integer format:"))
+
+Surface_area_of_a_cube=6*(a*a)
+print(Surface_area_of_a_cube)
+
+area_of_a_circle=(22/7)*(a*a)
+print(area_of_a_circle)
+
+l = int(input("enter the value in integer format:"))
+w = int(input("enter the value in integer format:"))
+h = int(input("enter the value in integer format:"))
+
+Surface_area_of_a_cuboid=(2*l*w)+(2*l*h)+(2*h*w)
+print(Surface_area_of_a_cuboid)
+
+r = int(input("enter the value in integer format:"))
+h = int(input("enter the value in integer format:"))
+
+area_of_a_cylinder=(2*(22/7)*r*h) + (2*(22/7)*r*r)
+print(area_of_a_cylinder)
+
+r = int(input("enter the value in integer format:"))
+h = int(input("enter the value in integer format:"))
+
+area_of_a_cone=((22/7)*r)*(r+((h*h)+(r*r))**(1/2))
+print(area_of_a_cone)
+
+r = int(input("enter the value in integer format:"))
+diameter_of_a_circle=2*r
+print(diameter_of_a_circle)
+
+s = int(input("enter the value in integer format:"))
+volume_of_a_square=s*s*s
+print(volume_of_a_square)
+
+area_of_a_square=s*s
+print(area_of_a_square)
+
+w = int(input("enter the value in integer format:"))
+l = int(input("enter the value in integer format:"))
+h = int(input("enter the value in integer format:"))
+
+volume_of_a_rectangle=w*l
+print(volume_of_a_rectangle)
+
+area_of_a_rectangle=l*w*h
+print(area_of_a_rectangle)
+
+
+b = int(input("enter the value in integer format:"))
+h = int(input("enter the value in integer format:"))
+
+area_of_a_triangle=(b*h)/2
+print(area_of_a_triangle)
